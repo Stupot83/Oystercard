83c58943b614d520426cb8429ebc1ee1aa0d40ff
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-21 13:24:48 +0100

    :construction::white_check_mark: Add code to deduct from balance and pass all tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -22,4 +22,12 @@ describe Oystercard do
     subject.top_up(maximum_balance)
     expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
   end
+
+  describe "#deduct" do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+  end
+
+  it "can deduct from the balance" do
+    expect { subject.deduct 1 }.to change { subject.balance }.by -1
+  end
 end
